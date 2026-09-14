dd3c16e4c8fbdadcaa7c2cf70a33223ab58b3da6
MikuDev <axfootballcards@gmail.com>
2026-09-14 12:38:41 +0200

    Update OS version to MicroOS C 1.9.0

--- sys/fetchvars.h
+++ sys/fetchvars.h
@@ -1,7 +1,7 @@
 #ifndef FETCHVARS_H
 #define FETCHVARS_H
 
-const char *os = "MicroOS C 1.8.0 (686)\n";
+const char *os = "MicroOS C 1.9.0 (686)\n";
 const char *kernel = "NM Kernel (New Micro Kernel) 1.0.0\n";
 const char *fetch_arch = "686 (32-bit)\n";
 
